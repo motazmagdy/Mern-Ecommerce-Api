739fc0846fa5d3bd795a5ef5a4443b6fad9b95b3
Motaz <eng.motaz.magdy@gmail.com>
2023-03-11 17:41:10 +0200

    connecting MongoDB

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express')
 
 const app = express()
+const {MongoClient} = require('mongodb')
+let db 
 
-app.get('/',(req,res)=>{
-    res.send('Home page from Node & Express')
+app.get('/', async (req,res)=>{
+    const allData = await db.collection('animals').find().toArray()
+
+    console.log(allData);
+    res.send(allData)
 })
 
-app.get('/admin',(req,res)=>{
-    res.send('Admin page from Node & Express')
+app.get('/admin', async (req,res)=>{
+    // res.send(allData)
 })
 
-app.listen(3000,()=>console.log('Server Running'))
+async function connectMongoDb (){
+    const client = new MongoClient('mongodb+srv://motaz:motazadmin@ecommerce.tpldgax.mongodb.net/?retryWrites=true&w=majority')
+    await client.connect()
+    db = client.db('MernApp')
+    console.log('Connected to DB');
+    app.listen(3000,()=>console.log('Server Running'))
+
+}
+connectMongoDb()
