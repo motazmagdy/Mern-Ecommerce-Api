8216f7414828bce4dc0adba5d169e5ae7a247fbc
Motaz <eng.motaz.magdy@gmail.com>
2023-03-11 23:15:54 +0200

    before Mongoose & Implementing  adding new item

--- server.js
+++ server.js
@@ -1,19 +1,27 @@
 const express = require('express')
-
 const app = express()
+var cors = require('cors')
 const {MongoClient} = require('mongodb')
-let db 
+let db ;
+
+app.use(cors())
+
+app.set("view engine","ejs")
+app.set("views","./views")
+app.use(express.static('public'))
 
 app.get('/', async (req,res)=>{
     const allData = await db.collection('animals').find().toArray()
-
-    console.log(allData);
     res.send(allData)
 })
 
 app.get('/admin', async (req,res)=>{
-    // res.send(allData)
+    const allData = await db.collection('animals').find().toArray()
+    res.json(allData)
 })
+
+// app.get('/api/items', async (req,res)=>{
+// })
 
 async function connectMongoDb (){
     const client = new MongoClient('mongodb+srv://motaz:motazadmin@ecommerce.tpldgax.mongodb.net/?retryWrites=true&w=majority')
